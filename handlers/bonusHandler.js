9039293c2b9e7f4f42140d1b7bdcd5c90f35f0e3
macbook <macbook@MacBook-Air.local>
2025-04-15 03:24:47 +0500

    ishlasinda endi

--- handlers/bonusHandler.js
+++ handlers/bonusHandler.js
@@ -1,8 +1,4 @@
-/** @format */
-
-// handlers/bonusHandler.js
-const userStates = require("../userStates");
-const showBonusCard = require("../components/showBonusCard");
+const UserState = require("../models/UserState"); // Yoki alohida helpers/state.js dagi getOrCreateUserState
 
 module.exports = (bot) => {
   bot.on("callback_query", async (query) => {
@@ -10,21 +6,26 @@ module.exports = (bot) => {
     const data = query.data;
 
     if (data === "check_balance") {
-      const userCode = userStates[chatId]?.userCode;
-      if (!userCode) {
+      // Avval DB dan foydalanuvchi holatini olamiz:
+      const state = await UserState.findOne({ chatId });
+      // Yoki: const state = await getOrCreateUserState(chatId);
+
+      if (!state || !state.userCode) {
         return bot.sendMessage(
           chatId,
           "❗ Siz hali ro‘yxatdan o‘tmagansiz. /start buyrug‘ini bosing."
         );
       }
 
+      // Agar userCode mavjud bo‘lsa, oldingi xabarni o'chirishga harakat qilamiz (ixtiyoriy)
       try {
         await bot.deleteMessage(chatId, query.message.message_id);
       } catch (e) {
         console.log("Oldingi xabarni o‘chirishda xatolik:", e.message);
       }
 
-      await showBonusCard(bot, chatId, userCode);
+      // Endi bonus kartani qayta ko‘rsatish
+      await showBonusCard(bot, chatId, state.userCode);
     }
   });
 };
